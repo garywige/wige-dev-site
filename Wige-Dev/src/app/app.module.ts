f33b1d63103f6c43df4d367bff210c1f89afa039
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-11 14:34:01 -0800

    Added flex and catch phrase, a little color

--- Wige-Dev/src/app/app.module.ts
+++ Wige-Dev/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { MaterialModule } from './material-module/material.module';
 import { NgModule } from '@angular/core';
 
@@ -13,7 +14,8 @@ import { NgModule } from '@angular/core';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
