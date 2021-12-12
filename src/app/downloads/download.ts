6b7dc24f9cb2941fe87a6c1646b4b9acdf5f84ae
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 15:43:06 -0800

    Download Class
    as well as an array in the Downloads component, partially implemented for the purpose of replacing the current contents of the dl

---
+++ src/app/downloads/download.ts
@@ -0,0 +1,11 @@
+export class Download {
+  name: string
+  url: string
+  description: string
+
+  constructor(){
+    this.name = ''
+    this.url = ''
+    this.description = ''
+  }
+}
--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Download } from './download'
+
 @Component({
   selector: 'app-downloads',
   templateUrl: './downloads.component.html',
   styleUrls: ['./downloads.component.css']
 })
 export class DownloadsComponent implements OnInit {
+  downloads: Download[]
 
-  constructor() { }
+  constructor() {
+    this.downloads = [
+      {name: 'Item 1', url: 'https://wige-dev.com', description: 'Lorem ipsum dolor sit amet'},
+      {name: 'Item 2', url: 'https://wige-dev.com', description: 'Lorem ipsum dolor sit amet'}
+    ]
+  }
 
   ngOnInit(): void {
   }
