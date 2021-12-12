c2f0d93debba454f397ff368f44a5eae0d22249b
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 11:05:19 -0800

    Renamed material-module folder
    to just 'material'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { MaterialModule } from './material-module/material.module'
+import { MaterialModule } from './material/material.module'
 
 @NgModule({
   declarations: [AppComponent],
