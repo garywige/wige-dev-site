729285944235c17ff82afe4e282b5a85eb850ce2
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 15:15:15 -0800

    Search Input
    Adjusted appearance of input on the Downloads page. This included some additions to the material module and I also attempted to fix some VSCode tooling.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-let modules = [MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule]
+let modules = [MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule, MatInputModule]
 
 @NgModule({
   imports: modules,
