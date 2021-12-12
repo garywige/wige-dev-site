5b8ff92265fa9257f0b04955f108407ccbc3b0dd
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-11 20:34:46 -0800

    Fix Lint/Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
-import { MaterialModule } from './material-module/material.module';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { MaterialModule } from './material-module/material.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/material-module/material.module.ts
+++ src/app/material-module/material.module.ts
@@ -1,12 +1,12 @@
+import { NgModule } from '@angular/core'
 import { MatIconModule } from '@angular/material/icon'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { NgModule } from '@angular/core'
 
 let modules = [MatToolbarModule, MatIconModule, MatMenuModule]
 
 @NgModule({
   imports: modules,
-  exports: modules
+  exports: modules,
 })
-export class MaterialModule { }
+export class MaterialModule {}
