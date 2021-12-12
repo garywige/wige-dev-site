eec438a7684f0b3810542b58b9b7c6437b4927b6
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 14:42:08 -0800

    Downloads Basic Layout
    markup representing basic layout, imported MatCardModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-let modules = [MatToolbarModule, MatIconModule, MatMenuModule]
+let modules = [MatToolbarModule, MatIconModule, MatMenuModule, MatCardModule]
 
 @NgModule({
   imports: modules,
