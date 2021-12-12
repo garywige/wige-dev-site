757d0aa72547903f7515b0ca72709800802318df
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 13:09:21 -0800

    Modified Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+import { DownloadsComponent } from './downloads/downloads.component'
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'downloads', component: DownloadsComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
