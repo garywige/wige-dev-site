d46c5cb58aab16f15f7d70d5d2e942a34934a8dd
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 17:05:25 -0800

    Imported assets
    Imported download assets into the project, modified sample data so it reflects the downloadable assets imported.

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -13,8 +13,10 @@ export class DownloadsComponent implements OnInit {
 
   constructor() {
     this.downloads = [
-      {name: 'Item 1', url: 'https://wige-dev.com', description: 'Lorem ipsum dolor sit amet'},
-      {name: 'Item 2', url: 'https://wige-dev.com', description: 'Lorem ipsum dolor sit amet'}
+      {name: 'AXICOM Copy', url: './assets/AXICOMCopy-v1.1.zip',
+        description: 'simple batch file copy tool for IT admins'},
+      {name: 'WoL Tool', url: './assets/WoL_Tool-0.2.0.1.zip',
+      description: 'use Wake-on-LAN to wake a machine up on the local network'}
     ]
 
     this.search = ''
