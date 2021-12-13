d66314c87007c83b825502f20f66a11bf5d2cfde
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-12 19:57:06 -0800

    ContactUs Component
    Added new component, added to routes, and configured nav link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { ContactUsComponent } from './contact-us/contact-us.component'
 import { DownloadsComponent } from './downloads/downloads.component'
 import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'downloads', component: DownloadsComponent },
+  { path: 'contact-us', component: ContactUsComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { DownloadsComponent } from './downloads/downloads.component'
 import { HomeComponent } from './home/home.component'
-import { MaterialModule } from './material/material.module'
+import { MaterialModule } from './material/material.module';
+import { ContactUsComponent } from './contact-us/contact-us.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DownloadsComponent],
+  declarations: [AppComponent, HomeComponent, DownloadsComponent, ContactUsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
