6eaf26f883858f56b83d9bb0dc8cf1a19e997e21
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-18 11:03:56 -0800

    Implemented Cancel Button

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
   form: FormGroup
@@ -13,11 +13,18 @@ export class ContactUsComponent implements OnInit {
     this.form = new FormGroup({
       email: new FormControl(''),
       subject: new FormControl(''),
-      message: new FormControl('')
+      message: new FormControl(''),
     })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onCancel() {
+    // clear form
+    this.form.patchValue({
+      email: '',
+      subject: '',
+      message: '',
+    })
+  }
 }
