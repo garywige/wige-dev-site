dd54657b7072f9d8479d48574c8acccc34e03dd6
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-01-05 19:40:18 -0800

    css: fixed toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [],
+  styles: [
+    `
+      .fixed-bar {
+        position: fixed;
+        top: 0;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
