00acbcd7a425f56ef5042e8c30740c7524c5a490
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-01-23 18:02:09 -0800

    modified distFolder path in server.ts

--- server.ts
+++ server.ts
@@ -1,17 +1,18 @@
 import 'zone.js/dist/zone-node'
 
-import { ngExpressEngine } from '@nguniversal/express-engine'
-import * as express from 'express'
+import { existsSync } from 'fs'
 import { join } from 'path'
 
-import { AppServerModule } from './src/main.server'
 import { APP_BASE_HREF } from '@angular/common'
-import { existsSync } from 'fs'
+import { ngExpressEngine } from '@nguniversal/express-engine'
+import * as express from 'express'
+
+import { AppServerModule } from './src/main.server'
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express()
-  const distFolder = join(process.cwd(), 'dist/Wige-Dev/browser')
+  const distFolder = join(process.cwd(), 'dist/Wige-Dev')
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? 'index.original.html'
     : 'index'
